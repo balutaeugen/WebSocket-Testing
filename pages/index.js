85536a8a4045b809867d2be72ea4b78b24123358
Baluta Eugen <30663151+balutaeugen@users.noreply.github.com>
2024-07-18 14:50:56 +0300

    Added Websockets

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,21 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 8080 });
 
 export default function Home() {
+
+
+  wss.on('connection', function connection(ws) {
+    ws.on('message', function message(data) {
+      console.log('received: %s', data);
+    });
+
+    ws.send('something');
+  });
+
   return (
     <div className="container">
       <Head>
